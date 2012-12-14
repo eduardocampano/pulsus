b410d28ffcb97b78d0a423d288282694c51200db
eduardocampano <campano.eduardo@gmail.com>
2012-12-14 15:23:38 -0300

    Added Pulsus.Web for .NET 4 dynamic module registration in webforms

---
+++ src/Pulsus.Web/AppStart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+namespace Pulsus.Web
+{
+	public static class AppStart
+	{
+		public static void Start()
+		{
+			DynamicModuleUtility.RegisterModule(typeof(ErrorLoggingModule));
+		}
+	}
+}

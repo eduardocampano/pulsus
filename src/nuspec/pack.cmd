b3f2a014272fd79efcaa18a3dd828825f6f8fe7c
eduardocampano <campano.eduardo@gmail.com>
2012-12-18 17:16:54 -0300

    Checked nuspecs and pack

--- src/nuspec/pack.cmd
+++ src/nuspec/pack.cmd
@@ -12,11 +12,16 @@ copy ..\Pulsus.Mvc\bin\Release\Pulsus.Mvc.dll Packages\Pulsus.Mvc\lib\net4\
 copy ..\Pulsus.Mvc\bin\Release\Pulsus.Mvc.pdb Packages\Pulsus.Mvc\lib\net4\
 REM copy ..\Pulsus.Mvc\bin\Release\Pulsus.Mvc.xml Packages\Pulsus.Mvc\lib\net4\
 
+copy ..\Pulsus.Web\bin\Release\Pulsus.Web.dll Packages\Pulsus.Web\lib\net4\
+copy ..\Pulsus.Web\bin\Release\Pulsus.Web.pdb Packages\Pulsus.Web\lib\net4\
+REM copy ..\Pulsus.Web\bin\Release\Pulsus.Web.xml Packages\Pulsus.Web\lib\net4\
+
 copy ..\Pulsus.Server\bin\Release\Pulsus.Server.dll Packages\Pulsus.Server\lib\net45\
 copy ..\Pulsus.Server\bin\Release\Pulsus.Server.pdb Packages\Pulsus.Server\lib\net45\
 REM copy ..\Pulsus.Server\bin\Release\Pulsus.Server.xml Packages\Pulsus.Server\lib\net45\
 
 ..\..\tools\.nuget\nuget.exe update -self
 ..\..\tools\.nuget\nuget.exe pack Pulsus.nuspec -BasePath Packages\Pulsus -Output Packages
+..\..\tools\.nuget\nuget.exe pack Pulsus.Web.nuspec -BasePath Packages\Pulsus.Web -Output Packages
 ..\..\tools\.nuget\nuget.exe pack Pulsus.Mvc.nuspec -BasePath Packages\Pulsus.Mvc -Output Packages
 ..\..\tools\.nuget\nuget.exe pack Pulsus.Server.nuspec -BasePath Packages\Pulsus.Server -Output Packages

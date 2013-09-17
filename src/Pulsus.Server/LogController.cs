839a2c4a2ed13cc90f74d92d24f133b7447899f0
eduardocampano <campano.eduardo@gmail.com>
2013-09-17 16:10:39 -0300

    Updated Pulsus.Server references and assembly info

--- src/Pulsus.Server/LogController.cs
+++ src/Pulsus.Server/LogController.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,7 +9,7 @@ namespace Pulsus.Server
 {
 	public class LogController : ApiController
 	{
-		public HttpResponseMessage Get([FromUri] LoggingEvent loggingEvent, [FromUri] string apiKey)
+		public HttpResponseMessage Get([FromUri] LoggingEvent loggingEvent)
 		{
 			loggingEvent.MachineName = EnvironmentHelpers.TryGetMachineName();
 			loggingEvent.Count = 1;
@@ -55,18 +53,6 @@ namespace Pulsus.Server
 
 			if (loggingEvent.Date == DateTime.MinValue)
 				loggingEvent.Date = DateTime.UtcNow;
-		}
-
-		protected string GetHeader(string name, bool mandatory = true)
-		{
-			IEnumerable<string> values;
-			if (ControllerContext.Request.Headers.TryGetValues(name, out values))
-				return values.FirstOrDefault();
-
-			if (mandatory)
-				ModelState.AddModelError(name, string.Format(CultureInfo.InvariantCulture, "The {0} header is invalid", name));
-
-			return null;
 		}
 	}
 }

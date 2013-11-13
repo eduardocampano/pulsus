c6facfd362b6e34990fd44b8f6e984d5d7f1b713
brjenkins <brian.r.jenkins@gmail.com>
2013-11-13 15:48:42 -0500

    ULS target fix

--- src/Pulsus.SharePoint/2010/Features/Pulsus Module/Pulsus Module.EventReceiver.cs
+++ src/Pulsus.SharePoint/2010/Features/Pulsus Module/Pulsus Module.EventReceiver.cs
@@ -6,67 +6,73 @@ using Microsoft.SharePoint.Administration;
 
 namespace Pulsus.SharePoint.Features.Pulsus
 {
-    [Guid("ef7da16a-e11a-4444-a521-cc1fc1e3a78f")]
-    public class PulsusFeatureEventReceiver : SPFeatureReceiver
-    {
-        private const string Owner = "Pulsus";
+	[Guid("ef7da16a-e11a-4444-a521-cc1fc1e3a78f")]
+	public class PulsusFeatureEventReceiver : SPFeatureReceiver
+	{
+		private const string Owner = "Pulsus";
 
-        private static readonly SPWebConfigModification[] WebConfigModifications = 
-        {
-            new SPWebConfigModification() 
-            { 
-                Name = "add[@name='PulsusErrorLoggingModule']", 
-                Owner = Owner, 
-                Type = SPWebConfigModification.SPWebConfigModificationType.EnsureChildNode, 
-                Path = "/configuration/system.web/httpModules", 
-                Value = "<add name=\"PulsusErrorLoggingModule\" type=\"" + typeof(ErrorLoggingModule).AssemblyQualifiedName + "\" />"
-            },
-            new SPWebConfigModification() { 
-                Name = "add[@name='PulsusErrorLoggingModule']", 
-                Owner = Owner, 
-                Type = SPWebConfigModification.SPWebConfigModificationType.EnsureChildNode, 
-                Path = "/configuration/system.webServer/modules", 
-                Value = "<add name=\"PulsusErrorLoggingModule\" preCondition=\"integratedMode\" type=\"" + typeof(ErrorLoggingModule).AssemblyQualifiedName + "\" />"
-            }
-        };
+		private static readonly SPWebConfigModification[] WebConfigModifications = 
+		{
+			new SPWebConfigModification() 
+			{ 
+				Name = "add[@name='PulsusErrorLoggingModule']", 
+				Owner = Owner, 
+				Type = SPWebConfigModification.SPWebConfigModificationType.EnsureChildNode, 
+				Path = "/configuration/system.web/httpModules", 
+				Value = "<add name=\"PulsusErrorLoggingModule\" type=\"" + typeof(ErrorLoggingModule).AssemblyQualifiedName + "\" />"
+			},
+			new SPWebConfigModification() { 
+				Name = "add[@name='PulsusErrorLoggingModule']", 
+				Owner = Owner, 
+				Type = SPWebConfigModification.SPWebConfigModificationType.EnsureChildNode, 
+				Path = "/configuration/system.webServer/modules", 
+				Value = "<add name=\"PulsusErrorLoggingModule\" preCondition=\"integratedMode\" type=\"" + typeof(ErrorLoggingModule).AssemblyQualifiedName + "\" />"
+			},
+			new SPWebConfigModification() { 
+				Name = "add[@assembly='" + typeof(PulsusFeatureEventReceiver).Assembly.FullName +"']", 
+				Owner = Owner, 
+				Type = SPWebConfigModification.SPWebConfigModificationType.EnsureChildNode, 
+				Path = "/configuration/system.web/compilation/assemblies",
+				Value = "<add assembly=\"" + typeof(PulsusFeatureEventReceiver).Assembly.FullName + "\" /> "}
+		};
 
-        public override void FeatureActivated(SPFeatureReceiverProperties properties)
-        {
-            AddWebConfigModifications(properties);
-        }
+		public override void FeatureActivated(SPFeatureReceiverProperties properties)
+		{
+			AddWebConfigModifications(properties);
+		}
 
-        public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
-        {
-            RemoveWebConfigModifications(properties);
-        }
+		public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+		{
+			RemoveWebConfigModifications(properties);
+		}
 
-        private static void AddWebConfigModifications(SPFeatureReceiverProperties properties)
-        {
-            var application = (SPWebApplication)properties.Feature.Parent;
+		private static void AddWebConfigModifications(SPFeatureReceiverProperties properties)
+		{
+			var application = (SPWebApplication)properties.Feature.Parent;
 
-            foreach (var modification in WebConfigModifications)
-                application.WebConfigModifications.Add(modification);
+			foreach (var modification in WebConfigModifications)
+				application.WebConfigModifications.Add(modification);
 
-            application.WebService.ApplyWebConfigModifications();
-            application.Update();
-        }
+			application.WebService.ApplyWebConfigModifications();
+			application.Update();
+		}
 
-        private static void RemoveWebConfigModifications(SPFeatureReceiverProperties properties)
-        {
-            var application = (SPWebApplication)properties.Feature.Parent;
+		private static void RemoveWebConfigModifications(SPFeatureReceiverProperties properties)
+		{
+			var application = (SPWebApplication)properties.Feature.Parent;
 
-            var modificationsToRemove = new List<SPWebConfigModification>();
-            foreach (var modification in application.WebConfigModifications)
-            {
-                if (modification.Owner == Owner)
-                    modificationsToRemove.Add(modification);
-            }
+			var modificationsToRemove = new List<SPWebConfigModification>();
+			foreach (var modification in application.WebConfigModifications)
+			{
+				if (modification.Owner == Owner)
+					modificationsToRemove.Add(modification);
+			}
 
-            foreach (var modification in modificationsToRemove)
-                application.WebConfigModifications.Remove(modification);
+			foreach (var modification in modificationsToRemove)
+				application.WebConfigModifications.Remove(modification);
 
-            application.WebService.ApplyWebConfigModifications();
-            application.Update();
-        }
-    }
+			application.WebService.ApplyWebConfigModifications();
+			application.Update();
+		}
+	}
 }

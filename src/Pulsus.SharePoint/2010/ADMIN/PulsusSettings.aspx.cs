a1b6a94532671bfa5f8d25bd34f67693af7f9ad2
brjenkins <brian.r.jenkins@gmail.com>
2013-11-13 12:22:15 -0500

    Updated Settings page to update Pulsus config in all zones for an application

--- src/Pulsus.SharePoint/2010/ADMIN/PulsusSettings.aspx.cs
+++ src/Pulsus.SharePoint/2010/ADMIN/PulsusSettings.aspx.cs
@@ -8,67 +8,72 @@ using Microsoft.SharePoint.ApplicationPages;
 
 namespace Pulsus.SharePoint.ApplicationPages
 {
-    public partial class PulsusSettings : DialogAdminPageBase
-    {
-        private static readonly Guid PulsusFeatureId = new Guid("7465ed3a-f5cc-46ba-8205-0ba5fc0e8b35");
+	public partial class PulsusSettings : DialogAdminPageBase
+	{
+		private static readonly Guid PulsusFeatureId = new Guid("7465ed3a-f5cc-46ba-8205-0ba5fc0e8b35");
 
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-        }
-        
-        protected void Selector_ContextChange(object sender, EventArgs e)
-        {
-            var application = GetApplication();
-            var applicationConfig = GetApplicationConfig(application);
-            if (File.Exists(applicationConfig))
-                configuration.Text = File.ReadAllText(applicationConfig);
-        }
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+		}
+		
+		protected void Selector_ContextChange(object sender, EventArgs e)
+		{
+			var application = GetApplication();
+			var applicationConfig = GetApplicationConfig(application);
+			if (File.Exists(applicationConfig))
+				configuration.Text = File.ReadAllText(applicationConfig);
+		}
 
-        protected void okButton_Click(object sender, EventArgs e)
-        {
-            var application = GetApplication();
+		protected void okButton_Click(object sender, EventArgs e)
+		{
+			var application = GetApplication();
 
-            try
-            {
-                var xDocument = XDocument.Parse(configuration.Text);
-            }
-            catch (Exception)
-            {
-                // TODO: show error
-                LabelErrorMessage.Text = "Invalid XML condfiguration";
-                return;
-            }
-            
-            Enum.GetValues(typeof(SPUrlZone)).Cast<SPUrlZone>().ToList().ForEach(z =>
-            {
-                var applicationConfig = GetApplicationConfig(application);
-                File.WriteAllText(applicationConfig, configuration.Text);
-            });
+			try
+			{
+				var xDocument = XDocument.Parse(configuration.Text);
+			}
+			catch (Exception)
+			{
+				// TODO: show error
+				LabelErrorMessage.Text = "Invalid XML condfiguration";
+				return;
+			}
+			
+			Enum.GetValues(typeof(SPUrlZone)).Cast<SPUrlZone>().ToList().ForEach(z =>
+			{
+				var applicationConfig = GetApplicationConfig(application, z);
+				File.WriteAllText(applicationConfig, configuration.Text);
+			});
 
-            ReloadPulsusFeature(application);
-            
-            base.EndOperation(0);
-        }
+			ReloadPulsusFeature(application);
+			
+			base.EndOperation(0);
+		}
 
-        protected void ReloadPulsusFeature(SPWebApplication application)
-        {
-            application.Features.Add(PulsusFeatureId, true);
-        }
+		protected void ReloadPulsusFeature(SPWebApplication application)
+		{
+			application.Features.Add(PulsusFeatureId, true);
+		}
 
-        protected string GetApplicationConfig(SPWebApplication application)
-        {
-            return Path.Combine(application.GetIisSettingsWithFallback(SPUrlZone.Default).Path.FullName, "Pulsus.config");
-        }
+		protected string GetApplicationConfig(SPWebApplication application)
+		{
+			return GetApplicationConfig(application, SPUrlZone.Default);
+		}
 
-        protected SPWebApplication GetApplication()
-        {
-            var currentApplicationId = new Guid(Selector.CurrentId);
-            var application = SPWebService.ContentService.WebApplications.FirstOrDefault(a => a.Id.Equals(currentApplicationId));
-            if (application == null)
-                throw new Exception("Could not find application " + currentApplicationId);
+		protected string GetApplicationConfig(SPWebApplication application, SPUrlZone zone)
+		{
+			return Path.Combine(application.GetIisSettingsWithFallback(zone).Path.FullName, "Pulsus.config");
+		}
 
-            return application;
-        }
-    }
+		protected SPWebApplication GetApplication()
+		{
+			var currentApplicationId = new Guid(Selector.CurrentId);
+			var application = SPWebService.ContentService.WebApplications.FirstOrDefault(a => a.Id.Equals(currentApplicationId));
+			if (application == null)
+				throw new Exception("Could not find application " + currentApplicationId);
+
+			return application;
+		}
+	}
 }

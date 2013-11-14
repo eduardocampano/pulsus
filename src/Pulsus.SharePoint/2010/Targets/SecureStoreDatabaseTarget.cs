973fa9f4a61cd8ca727203beea2d0690a92ce117
brjenkins <brian.r.jenkins@gmail.com>
2013-11-13 19:09:32 -0500

    Changed access for the secure store to run with elevated permissions

--- src/Pulsus.SharePoint/2010/Targets/SecureStoreDatabaseTarget.cs
+++ src/Pulsus.SharePoint/2010/Targets/SecureStoreDatabaseTarget.cs
@@ -11,77 +11,83 @@ using Pulsus.Targets;
 
 namespace Pulsus.SharePoint.Targets
 {
-    public class SecureStoreDatabaseTarget : DatabaseTarget
-    {
-        public string AppId { get; set; }
+	public class SecureStoreDatabaseTarget : DatabaseTarget
+	{
+		public string AppId { get; set; }
 
-        public static SPServiceContext Context
-        {
-            get
-            {
-                return SPServiceContext.GetContext(SPServiceApplicationProxyGroup.Default, SPSiteSubscriptionIdentifier.Default);
-            }
-        }
+		public static SPServiceContext Context
+		{
+			get
+			{
+				return SPServiceContext.GetContext(SPServiceApplicationProxyGroup.Default, SPSiteSubscriptionIdentifier.Default);
+			}
+		}
 
-        protected override ConnectionSettings GetConnectionSettings()
-        {
-            if (string.IsNullOrEmpty(AppId))
-            {
-                PulsusDebugger.Error("The provided AppId is not valid. Please check configuration");
-                return null;
-            }
+		protected override ConnectionSettings GetConnectionSettings()
+		{
+			if (string.IsNullOrEmpty(AppId))
+			{
+				PulsusDebugger.Error("The provided AppId is not valid. Please check configuration");
+				return null;
+			}
 
-            var provider = SecureStoreProviderFactory.Create();
-            ((ISecureStoreServiceContext)provider).Context = Context;
+			ConnectionSettings connectionSettings = null;
+			SPSecurity.RunWithElevatedPrivileges(() =>
+			{
+				var provider = SecureStoreProviderFactory.Create();
+				((ISecureStoreServiceContext)provider).Context = Context;
 
-            using (var credentials = provider.GetCredentials(AppId))
-            {
-                if (credentials == null)
-                    throw new Exception(string.Format("Unable to retrive the credentials for AppId '{0}'", AppId));
+				using (var credentials = provider.GetCredentials(AppId))
+				{
+					if (credentials == null)
+						throw new Exception(string.Format("Unable to retrive the credentials for AppId '{0}'", AppId));
 
-                // used to validate connection string
-                var connectionStringBuilder = new DbConnectionStringBuilder();
+					// used to validate connection string
+					var connectionStringBuilder = new DbConnectionStringBuilder();
 
-                foreach (SecureStoreCredential credential in credentials)
-                {
-                    // as we don't have access to the credential name we can only evaluate the value
-                    // to find a connection string
+					foreach (SecureStoreCredential credential in credentials)
+					{
+						// as we don't have access to the credential name we can only evaluate the value
+						// to find a connection string
 
-                    if (credential.CredentialType == SecureStoreCredentialType.Generic)
-                    {
-                        var connectionString = ReadSecureString(credential.Credential);
+						if (credential.CredentialType == SecureStoreCredentialType.Generic)
+						{
+							var connectionString = ReadSecureString(credential.Credential);
 
-                        if (string.IsNullOrEmpty(connectionString))
-                            continue;
+							if (string.IsNullOrEmpty(connectionString))
+								continue;
 
-                        try
-                        {
-                            connectionStringBuilder.ConnectionString = connectionString;
-                        }
-                        catch (ArgumentException)
-                        {
-                            // this is not a connection string so move on
-                            continue;
-                        }
-                        
-                        return new ConnectionSettings(connectionString);
-                    }
-                }
-            }
+							try
+							{
+								connectionStringBuilder.ConnectionString = connectionString;
+							}
+							catch (ArgumentException)
+							{
+								// this is not a connection string so move on
+								continue;
+							}
 
-            PulsusDebugger.Error("Unable to find a connection string in AppId '{0}' credentials. Please check the secure store", AppId);
-            return null;
-        }
+							connectionSettings = new ConnectionSettings(connectionString);
+						}
+					}
+				}
+			});
 
-        protected string ReadSecureString(SecureString secureString)
-        {
-            if (secureString == null)
-                return null;
+			if (connectionSettings == null)
+				PulsusDebugger.Error("Unable to find a connection string in AppId '{0}' credentials. Please check the secure store", AppId);
 
-            var ptr = Marshal.SecureStringToBSTR(secureString);
-            var str = Marshal.PtrToStringBSTR(ptr);
-            Marshal.ZeroFreeBSTR(ptr);
-            return str;
-        }
-    }
+			return connectionSettings;
+		}
+
+		protected string ReadSecureString(SecureString secureString)
+		{
+			if (secureString == null)
+				return null;
+
+			var ptr = Marshal.SecureStringToBSTR(secureString);
+			var str = Marshal.PtrToStringBSTR(ptr);
+			Marshal.ZeroFreeBSTR(ptr);
+			return str;
+		}
+	}
 }

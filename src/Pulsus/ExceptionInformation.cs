d42edaa64b5e8a82fce680d2403776c39d704e29
Eduardo Campañó <campano.eduardo@gmail.com>
2013-12-09 17:55:18 -0300

    Source bug fix

--- src/Pulsus/ExceptionInformation.cs
+++ src/Pulsus/ExceptionInformation.cs
@@ -6,47 +6,54 @@ using Pulsus.Internal;
 
 namespace Pulsus
 {
-	public class ExceptionInformation
-	{
-	    public static ExceptionInformation Create(Exception exception)
-	    {
-	        return Create(exception, null);
-	    }
+    public class ExceptionInformation
+    {
+        public static ExceptionInformation Create(Exception exception)
+        {
+            return Create(exception, null);
+        }
 
-	    public static ExceptionInformation Create(Exception exception, string source)
-		{
-			// if it's not a .Net core exception, usually more information is being added
-			// so use the wrapper for the message, type, etc.
-			// if it's a .Net core exception type or an HttpUnhandledException, drill down and get the innermost exception
-			if (exception.IsBuiltInException() || exception.IsHttpUnhandledException())
-				exception = exception.GetBaseException();
+        public static ExceptionInformation Create(Exception exception, string source)
+        {
+            // if it's not a .Net core exception, usually more information is being added
+            // so use the wrapper for the message, type, etc.
+            // if it's a .Net core exception type or an HttpUnhandledException, drill down and get the innermost exception
+            if (exception.IsBuiltInException() || exception.IsHttpUnhandledException())
+                exception = exception.GetBaseException();
 
-			var exceptionInformation = new ExceptionInformation();
-			exceptionInformation.Message = exception.Message;
-			exceptionInformation.Type = exception.GetType().Name.Replace("Exception", string.Empty);
-			exceptionInformation.Source = string.Join(",", new[] { exception.Source ?? string.Empty, source ?? string.Empty });
-			exceptionInformation.More = new List<ExceptionInformation>();
+            var exceptionInformation = new ExceptionInformation();
+            exceptionInformation.Message = exception.Message;
+            exceptionInformation.Type = exception.GetType().Name.Replace("Exception", string.Empty);
 
-			var httpException = exception as HttpException;
-			if (httpException != null)
-				exceptionInformation.StatusCode = httpException.GetHttpCode();
-			else
-				exceptionInformation.StatusCode = 500;
+            var sources = new List<string>();
+            if (!exception.Source.IsNullOrEmpty())
+                sources.Add(exception.Source);
+            if (!source.IsNullOrEmpty())
+                sources.Add(source);
 
-			var reflectionTypeLoadException = exception as ReflectionTypeLoadException;
-			if (reflectionTypeLoadException != null)
-			{
-				foreach (var ex in reflectionTypeLoadException.LoaderExceptions)
-					exceptionInformation.More.Add(Create(ex));
-			}
+            exceptionInformation.Source = string.Join(",", sources.ToArray());
+            exceptionInformation.More = new List<ExceptionInformation>();
 
-			return exceptionInformation;
-		}
+            var httpException = exception as HttpException;
+            if (httpException != null)
+                exceptionInformation.StatusCode = httpException.GetHttpCode();
+            else
+                exceptionInformation.StatusCode = 500;
 
-		public string Message { get; set; }
-		public string Type { get; set; }
-		public string Source { get; set; }
-		public int StatusCode { get; set; }
-		public IList<ExceptionInformation> More { get; set; }
-	}
+            var reflectionTypeLoadException = exception as ReflectionTypeLoadException;
+            if (reflectionTypeLoadException != null)
+            {
+                foreach (var ex in reflectionTypeLoadException.LoaderExceptions)
+                    exceptionInformation.More.Add(Create(ex));
+            }
+
+            return exceptionInformation;
+        }
+
+        public string Message { get; set; }
+        public string Type { get; set; }
+        public string Source { get; set; }
+        public int StatusCode { get; set; }
+        public IList<ExceptionInformation> More { get; set; }
+    }
 }
